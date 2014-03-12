76fbecf2ee2f562c5e94e388f28fcc73d71276e9
Mac Clayton <mckenzieclayton@gmail.com>
2014-03-11 22:00:17 -0400

    updates

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -1,12 +1,19 @@
+var item = "hello";
 $(document).ready( function() {
-  $(".toggle-nav").click( function() {
-    var navigation = $("nav");
-
-    if (navigation.hasClass("visible")) {
-      navigation.removeClass("visible").addClass("hidden");
-    } else {
-      navigation.removeClass("hidden").addClass("visible");
-    }
-
-  });
+	$(".toggle-nav").click( function() {
+	var navigation = $("nav");
+	if (navigation.hasClass("visible")) {
+		navigation.removeClass("visible").addClass("hidden");
+	} else {
+		navigation.removeClass("hidden").addClass("visible");
+	}
+	});
+	$('.language-filter li').click(function() {
+		$("li.active").removeClass("active");
+		$(this).addClass('active');
+		item = "goodby";
+	});
+	console.log(item);
 });
+
+
